3f4968b6964fb5a3ceebba79cf6e29112621d7a7
li <li@goldragon.criome.net>
2026-06-18 19:11:54 +0200

    nota-next: use shared rust-build source policy

--- flake.nix
+++ flake.nix
@@ -4,33 +4,23 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    fenix = {
-      url = "github:nix-community/fenix";
+    rust-build = {
+      url = "github:LiGoldragon/rust-build";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    crane.url = "github:ipetkov/crane";
   };
 
-  outputs = { self, nixpkgs, flake-utils, fenix, crane }:
+  outputs = { self, nixpkgs, flake-utils, rust-build }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        toolchain = fenix.packages.${system}.stable.withComponents [
-          "cargo"
-          "rustc"
-          "rustfmt"
-          "clippy"
-          "rust-src"
-        ];
-        craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+        rust = rust-build.lib.${system}.fromPkgs pkgs;
+        inherit (rust) craneLib toolchain;
         notaFilter = path: type:
           type == "regular" && pkgs.lib.hasSuffix ".nota" path;
-        sourceFilter = path: type:
-          type == "directory" || (craneLib.filterCargoSources path type) || (notaFilter path type);
-        src = pkgs.lib.cleanSourceWith {
-          src = ./.;
-          filter = sourceFilter;
-          name = "source";
+        src = rust.cleanSource {
+          root = ./.;
+          extraFilters = [ notaFilter ];
         };
         commonArguments = { inherit src; strictDeps = true; };
         cargoArtifacts = craneLib.buildDepsOnly commonArguments;
